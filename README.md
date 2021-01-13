68c9d64a78700bdd116d0f2da8b562616959ba2d
Kazumasa Utashiro <kaz@utashiro.com>
2021-01-13 15:03:18 +0900

    minil new App::lms

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::lms - It's new $module
+
+# SYNOPSIS
+
+    use App::lms;
+
+# DESCRIPTION
+
+App::lms is ...
+
+# LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kazumasa Utashiro <kaz@utashiro.com>
---
+++ lib/App/lms.pm
@@ -0,0 +1,39 @@
+package App::lms;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::lms - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::lms;
+
+=head1 DESCRIPTION
+
+App::lms is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
